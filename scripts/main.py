1114b88d67de7f7bc110b5ab19c9b72848b3c33e
Furley <David.Furley@ons.gov.uk>
2022-08-09 15:53:38 +0100

    Created basic streamlit app for fk text analysis, outputting reading ease and grade level scores

--- scripts/main.py
+++ scripts/main.py
@@ -5,3 +5,13 @@ Created on Tue Aug  9 15:12:20 2022
 @author: furled
 """
 
+# import libraries
+import textstat as tx
+import streamlit as st
+
+st.title('ONS recruitment - readability scoring')
+
+text = st.text_area(label = 'Text to analyse', value = 'Input text to analyse here', height = 300)
+
+st.write('Reading ease: {:.2f}'.format(tx.flesch_reading_ease(text)))
+st.write('Grade level: {:.2f}'.format(tx.flesch_kincaid_grade(text)))
